b3369b22e5e22e657a1acc0815f87f7548e192fe
Eng-Khaled-Alhadi <adwhiteeagle@gmail.com>
2021-08-10 19:55:17 +0300

    Create RequestType.java

---
+++ Android/ChatRoom/app/src/main/java/com/terminus/chatroom/RequestType.java
@@ -0,0 +1,17 @@
+package com.terminus.chatroom;
+
+public class RequestType {
+    public static final int CONNECT_ID = 943;
+    public static final int NEW_FEE = 711;
+    public static final int NEW_MESSAGE = 906;
+    public static final int NEW_HOMEWORK = 543;
+    public static final int NEW_REFERENCE = 247;
+    public static final int NEW_ACTIVITIES = 564;
+    public static final int NEW_PUBLIC_NOTIFICATION = 823;
+    public static final int NEW_PUBLIC_NOTIFICATION_FOR_STUDENT = 541;
+    public static final int IS_CONNECTED = 284;
+    public static final int IS_NEW_HOMEWORK = 234;
+    public static final int IS_NEW_REFERENCE = 241;
+    public static final int CONFIRM_MESSAGE  = 156;
+
+}
