ae22c3fa401f657268a86de5365186808c4599f4
Eng-Khaled-Alhadi <adwhiteeagle@gmail.com>
2021-08-10 19:54:57 +0300

    Update MainActivity.java

--- Android/ChatRoom/app/src/main/java/com/terminus/chatroom/MainActivity.java
+++ Android/ChatRoom/app/src/main/java/com/terminus/chatroom/MainActivity.java
@@ -1,9 +1,7 @@
 package com.terminus.chatroom;
 
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,59 +11,90 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonObject;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.WebSocket;
-import okhttp3.WebSocketListener;
-import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebSocket webSocket;
+    //    private WebSocket webSocket;
+    public static MainSocketServer socketServer;
     private MessageAdapter adapter;
+    public static int userId = 0;
+    public static int toUid = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        socketServer = new MainSocketServer();
         ListView messageList = findViewById(R.id.messageList);
         final EditText messageBox = findViewById(R.id.messageBox);
         TextView send = findViewById(R.id.send);
 
-
-
-        instantiateWebSocket();
-
-
-
         adapter = new MessageAdapter();
         messageList.setAdapter(adapter);
 
+        MainActivity.socketServer.getListener().setOnMessage(new com.terminus.chatroom.SocketListener.OnMessage() {
+            @Override
+            public void onMessage(final Message message) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        JSONObject jsonObject = new JSONObject();
+
+                        try {
+
+
+                            jsonObject.put("message", message.getData());
+                            jsonObject.put("byServer", true);
+
+                            adapter.addItem(jsonObject);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
 
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+//                if (MainActivity.socketServer.send(new IsConnect(toUid, 0))) {
+//                    Log.d("test_send","success");
+//
+//                }else {
+//
+//                    Log.d("test_send","filed");
+//                }
 
                 String message = messageBox.getText().toString();
 
 
-
                 if (!message.isEmpty()) {
 
+                    Message message1 = new Message(
+                            userId, toUid, message, 1, String.valueOf(Calendar.getInstance().getTimeInMillis()), 0
+                    );
+                    if (MainActivity.socketServer.send(message1)) {
+                        messageBox.setText("");
 
-                    webSocket.send(message);
-                    messageBox.setText("");
-
+                    } else {
+                        Toast.makeText(MainActivity.this, "cant'n send", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
 
                     JSONObject jsonObject = new JSONObject();
@@ -73,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                     try {
 
 
-                        jsonObject.put("message", message);
+                        jsonObject.put("message", message1.getData());
                         jsonObject.put("byServer", false);
 
                         adapter.addItem(jsonObject);
@@ -87,127 +116,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        MainActivity.socketServer.getListener().setOnConfirmMessage(new SocketListener.OnConfirmMessage() {
+            @Override
+            public void onConfirmMessage(final String date) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, date, Toast.LENGTH_SHORT).show();
 
-
-    }
-
-
-
-
-    private void instantiateWebSocket() {
-
-
-
-        OkHttpClient client = new OkHttpClient();
-
-
-        //replace x.x.x.x with your machine's IP Address
-        Request request = new Request.Builder().url("ws://x.x.x.x:8080").build();
-
-
-        SocketListener socketListener = new SocketListener(this);
-
-
-        webSocket = client.newWebSocket(request, socketListener);
-
-
-
-    }
-
-
-
-
-
-    public class SocketListener extends WebSocketListener {
-
-
-        public MainActivity activity;
-
-
-
-        public SocketListener(MainActivity activity) {
-            this.activity = activity;
-        }
-
-
-
-        @Override
-        public void onOpen(WebSocket webSocket, Response response) {
-
-
-            activity.runOnUiThread(new Runnable() {
-
-                @Override
-                public void run() {
-
-                    Toast.makeText(activity, "Connection Established!", Toast.LENGTH_LONG).show();
-
-                }
-
-            });
-
-        }
-
-        @Override
-        public void onMessage(WebSocket webSocket, final String text) {
-
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-
-                    JSONObject jsonObject = new JSONObject();
-
-                    try {
-
-
-                        jsonObject.put("message", text);
-                        jsonObject.put("byServer", true);
-
-                        adapter.addItem(jsonObject);
-
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
-                }
-            });
-        }
+                });
+            }
+        });
+        MainActivity.socketServer.getListener().setIsConnect(new SocketListener.OnIsConnect() {
+            @Override
+            public void isConnect(final int status) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getSupportActionBar().setTitle(status > 0 ? "online" : "offline");
+                    }
+                });
+            }
+        });
 
 
+        MainActivity.socketServer.getListener().setOnNotification(new SocketListener.OnNotification() {
+            @Override
+            public void onNotification(final MyNotification notification) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, notification.getTitle(), Toast.LENGTH_SHORT).show();
 
-        @Override
-        public void onMessage(WebSocket webSocket, ByteString bytes) {
-            super.onMessage(webSocket, bytes);
-        }
-
-
-
-        @Override
-        public void onClosing(WebSocket webSocket, int code, String reason) {
-            super.onClosing(webSocket, code, reason);
-        }
-
-
-
-        @Override
-        public void onClosed(WebSocket webSocket, int code, String reason) {
-            super.onClosed(webSocket, code, reason);
-        }
+                    }
+                });
+            }
+        });
 
 
-
-        @Override
-        public void onFailure(WebSocket webSocket, final Throwable t, @Nullable final Response response) {
-            super.onFailure(webSocket, t, response);
-        }
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MainActivity.socketServer.onDestroy();
+    }
+
 
     public class MessageAdapter extends BaseAdapter {
 
 
-
         List<JSONObject> messagesList = new ArrayList<>();
-
 
 
         @Override
@@ -272,7 +233,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
         void addItem(JSONObject item) {
 
 
@@ -281,7 +241,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
-
 
 
     }
